993324976d3b6aae82b1a8f30847d5d3f03d6620
priyagund <priyankagund18@gmail.com>
2019-11-11 16:30:00 +0530

    calculated first use case a+b*c

--- ProblemStatment1.sh
+++ ProblemStatment1.sh
@@ -4,4 +4,6 @@ read -p "Enter the value for a: " a
 read -p "Enter the value for b: " b
 read -p "Enter the value for c: " c
 
+declare -A resultDictionary
+resultDictionary["a+b*c"]=$(($a+$b*$c))
 
