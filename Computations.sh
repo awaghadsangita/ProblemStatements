1215c9873bc73df782692cbea6370bfa6c02b2b3
priyagund <priyankagund18@gmail.com>
2019-11-12 10:38:45 +0530

    added fourth use case a*b+c

--- Computations.sh
+++ Computations.sh
@@ -10,26 +10,28 @@ result2=$(($c+$a/$b))
 echo $result2
 result3=$(($a%$b+$c))
 echo $result3
-
+result4=$(($a*$b+$c))
 declare -A resultDictionary
 resultDictionary["a+b*c"]=$result1
 resultDictionary["c+a/b"]=$result2
 resultDictionary["a%b+c"]=$result3
+resultDictionary["a*b+c"]=$result4
 echo "Result From Dictionary :" ${resultDictionary[@]}
 
 couter=0
 resultArray[((counter++))]=${resultDictionary["a+b*c"]}
 resultArray[((counter++))]=${resultDictionary["c+a/b"]}
 resultArray[((counter++))]=${resultDictionary["a%b+c"]}
+resultArray[((counter++))]=${resultDictionary["a*b+c"]}
 echo "Result From Array :"${resultArray[@]}
 
 function sortDescending()
 {
 	resultArray=$1;
-	for ((i = 0; i<2; i++)) 
+	for ((i = 0; i<3; i++)) 
 	do
       
-    		for ((j = i; j<3-i; j++)) 
+    		for ((j = i; j<4-i; j++)) 
     		do
       
         		if [ ${resultArray[j]} -lt ${resultArray[$((j+1))]} ]
@@ -49,10 +51,10 @@ echo "Sorted Array in Descending Order :"$( sortDescending $resultArray )
 function sortAscending()
 {
 	resultArray=$1;
-	for ((i = 0; i<2; i++)) 
+	for ((i = 0; i<3; i++)) 
 	do
       
-    		for ((j = i; j<3-i; j++)) 
+    		for ((j = i; j<4-i; j++)) 
     		do
       
         		if [ ${resultArray[j]} -gt ${resultArray[$((j+1))]} ]
